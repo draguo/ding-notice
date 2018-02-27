e5ec5b531d13b2419b9f374abd7c15aabf96f4cd
draguo <draguo@sina.com>
2018-02-27 22:04:26 +0800

    完善测试

--- tests/ActionCardTest.php
+++ tests/ActionCardTest.php
@@ -7,7 +7,8 @@ use Draguo\Ding\Message\ActionCard;
 
 class ActionCardTest extends TestCase
 {
-    public function testSend()
+    // 整体跳转
+    public function testJumpAllSend()
     {
         $card = new ActionCard([
             "title" => "乔布斯 20 年前想打造一间苹果咖啡厅，而它正是 Apple Store 的前身",
@@ -18,6 +19,32 @@ class ActionCardTest extends TestCase
             "btnOrientation" => "0",
             "singleTitle" => "阅读全文",
             "singleURL" => "https://www.dingtalk.com/"
+        ]);
+
+        $result = $this->app->send($card);
+
+        $this->assertEquals(0, $result->errcode);
+    }
+
+    public function testJumpEveryOne()
+    {
+        $card = new ActionCard([
+            "title" => "乔布斯 20 年前想打造一间苹果咖啡厅，而它正是 Apple Store 的前身",
+            "text" => "![screenshot](@lADOpwk3K80C0M0FoA) 
+               ### 乔布斯 20 年前想打造的苹果咖啡厅 
+                Apple Store 的设计正从原来满满的科技感走向生活化，而其生活化的走向其实可以追溯到 20 年前苹果一个建立咖啡馆的计划",
+            "hideAvatar" => "0",
+            "btnOrientation" => "0",
+            "btns" => [
+                [
+                    "title" => "内容不错",
+                    "actionURL" => "https://www.dingtalk.com/"
+                ],
+                [
+                    "title" => "不感兴趣",
+                    "actionURL" => "https://www.dingtalk.com/"
+                ]
+            ]
         ]);
 
         $result = $this->app->send($card);
--- tests/MarkdownTest.php
+++ tests/MarkdownTest.php
@@ -7,17 +7,41 @@ use Draguo\Ding\Message\Markdown;
 
 class MarkdownTest extends TestCase
 {
-    public function testSend()
+    private function generateContent()
     {
-        $markdown = new Markdown([
+        // @ 单个人的时候要求在文字内也要有对应的手机号
+        return new Markdown([
             "title" => "杭州天气",
-            "text" => "#### 杭州天气 @156xxxx8827\n".
+            "text" => "#### 杭州天气 @{$this->testUser}\n".
                 "> 9度，西北风1级，空气良89，相对温度73%\n\n".
                 "> ![screenshot](http://image.jpg)\n".
                 "> ###### 10点20分发布 [天气](http://www.thinkpage.cn/) \n"
         ]);
+    }
+
+    public function testSend()
+    {
+        $markdown = $this->generateContent();
 
         $result = $this->app->send($markdown);
+
+        $this->assertEquals(0, $result->errcode);
+    }
+
+    public function testToSomeUser()
+    {
+        $markdown = $this->generateContent();
+
+        $result = $this->app->to($this->testUser)->send($markdown);
+
+        $this->assertEquals(0, $result->errcode);
+    }
+
+    public function testToAll()
+    {
+        $markdown = $this->generateContent();
+
+        $result = $this->app->to('all')->send($markdown);
 
         $this->assertEquals(0, $result->errcode);
     }
