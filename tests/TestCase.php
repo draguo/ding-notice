0cdcdb1c9bad25869ee85f73a6432c52eda22199
draguo <draguo@sina.com>
2018-05-26 18:06:46 +0800

    token test

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -12,7 +12,7 @@ class TestCase extends PHPUnitTestCase
 
     public function __construct()
     {
-        $token = '4dc5ab05357974596a70ea5b34f62bb05b62ead9f299de556f02eeb7fca9c37c';
+        $token = getenv('token');
         $this->testUser = '18888888888';
         $app = new Notice($token);
         $this->app = $app;
